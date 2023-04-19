9b2ca2af0bdea89d9d96d1dbfa16dce36b9c4f92
obidjonov1 <obidjonov@naver.com>
2023-04-19 12:46:20 +0900

    feat: BRR: Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+const connectionString = process.env.MONGO_URL;
+
+mongoose.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, goose) => {
+    if (err) console.log("ERROR on connection MongoDB:");
+    else {
+      console.log("MongoDB connection succeed");
+
+      const server = require("./app");
+      // my localhost:3003
+      let PORT = process.env.PORT || 3003;
+      server.listen(PORT, function () {
+        console.log(`runnig syccessfuly ${PORT}, http://localhost:${PORT}`);
+      });
+    }
+  }
+);
