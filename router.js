f568fca0c0ebe71762b4fbb2df04cec612b595e2
obidjonov1 <obidjonov@naver.com>
2023-04-21 14:44:38 +0900

    feat: develop getAllProducts rest api business logic

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const memberController = require("./controllers/memeberController");
+const productController = require("./controllers/productController");
 
 /* *************************
  *      REST API          *
@@ -19,12 +20,10 @@ router.get(
 );
 
 // Ohters routers
-router.get("/menu", (req, res) => {
-  res.send("Menu sahifasidasiz");
-});
-
-router.get("/community", (req, res) => {
-  res.send("Jamiyat sahifasidasiz");
-});
+router.post(
+  "/products",
+  memberController.retrieveAuthMember,
+  productController.getAllProducts
+);
 
 module.exports = router;
